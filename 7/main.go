5abf7418e26529ecb9fa5b1a91a95f00972d4442
fmouse <fmouse@fmousedeMacBook-Pro.local>
2021-02-08 09:24:13 +0800

    doc:题解

--- 7/main.go
+++ 7/main.go
@@ -5,6 +5,13 @@ import (
 	"math"
 )
 
+/**
+因为题目中假设环境不允许存储 64 位整数，所以每次先检查是否会超出 int32 的范围
+math.MinInt32 = -2147483648
+math.MaxInt32 = 2147483647
+因为在计算过程中是将 x 一位一位拆解再计算的，所以将 MaxInt32 和 MinInt32 也分成两部分，这样就可以预判是否会超出 int32 的范围
+*/
+
 func reverse(x int) int {
 
 	ans := 0
@@ -31,9 +38,9 @@ func reverse(x int) int {
 }
 
 func main() {
-	/*fmt.Println(reverse(123))
+	fmt.Println(reverse(123))
 	fmt.Println(reverse(-123))
 	fmt.Println(reverse(120))
-	fmt.Println(reverse(0))*/
+	fmt.Println(reverse(0))
 	fmt.Println(reverse(1534236469))
 }
